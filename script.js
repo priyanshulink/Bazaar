2812c19ae282e68c5d7a2358c220fb5b89a3b7a0
priyanshulink <thakurpriyanshusingh3133@gmail.com>
2025-04-16 00:10:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js on log in button
+document.getElementById("loginbutton").addEventListener("click", function () {
+    window.location.href = "login.html"; // mention the location of page.
+  });
+  
