5fb5cc1b313483e1fe87cad0a01ee07a087acffd
priyanshulink <thakurpriyanshusingh3133@gmail.com>
2025-04-17 00:01:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,6 @@
 document.getElementById("loginbutton").addEventListener("click", function () {
     window.location.href = "login.html"; // mention the location of page.
   });
-  
+  document.getElementById("getStart").addEventListener("click", function(){
+    window.location.href = "getstarted.html";
+  });
